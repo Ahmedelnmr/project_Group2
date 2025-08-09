a6f6fc1b8d09174e5732d488b99bf70d3ca3a095
Ahmed Elnmer <153692962+Ahmedelnmr@users.noreply.github.com>
2025-08-09 15:12:24 +0300

    Update Book.cs

--- project_Group2/Book.cs
+++ project_Group2/Book.cs
@@ -2,6 +2,19 @@
 {
     public class Book
     {
-        
+        private int Id;
+        private string Title;
+        private string Author;
+        private bool IsAvailable = true;
+        public Book(int id, string title, string author)
+            {
+                Id = id;
+                Title = title;
+                Author = author;
+            }
+        public override string ToString()
+            {
+                return $" the id of Book is:{Id} and his title :{Title} and his author{Author} ";
+            }
     }
 }
