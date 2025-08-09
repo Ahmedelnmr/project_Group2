ec74a99f7e19099ead717b5c8f68695e9185994a
bassemaboulgheit <basem.abulgheit@gmail.com>
2025-08-09 19:06:52 +0300

    Update Member.cs

--- project_Group2/Member.cs
+++ project_Group2/Member.cs
@@ -2,6 +2,21 @@
 {
     public class Member
     {
-
+        public int Id;
+        public string Name;
+        public int[] borrowed_books;
+        public int count;
+        public Member(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            borrowed_books = new int[5] ;
+            count = 0;
+        }
+        
+        public override string ToString()
+        {
+            return $"ID : {Id} Name is : {Name} borrowed_books : {count}";
+        }
     }
 }
