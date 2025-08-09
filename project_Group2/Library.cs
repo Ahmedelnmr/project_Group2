2bf3150a76cb107dc023185a7558163d56f883d2
bassemaboulgheit <basem.abulgheit@gmail.com>
2025-08-09 19:28:56 +0300

    Update Library.cs

--- project_Group2/Library.cs
+++ project_Group2/Library.cs
@@ -2,6 +2,16 @@
 {
     public class Library
     {
-
+        Book[] books;
+        int books_count;
+        Member[] members;
+        int members_count;
+        public Library()
+        {
+            books = new Book[20];
+            members = new Member[20];
+            books_count = 0;
+            members_count = 0;
+        }
     }
 }
