d96a06cfea6e2325e15f860b490d8dc5013c2c53
Ahmed Hani <ahmed.h.m112001@gmail.com>
2025-08-10 00:25:40 +0300

    Update Class_Member

--- project_Group2/Member.cs
+++ project_Group2/Member.cs
@@ -1,22 +1,56 @@
 ﻿namespace project_Group2
 {
-    public class Member
-    {
-        public int Id;
-        public string Name;
-        public int[] borrowed_books;
-        public int count;
-        public Member(int id, string name)
-        {
-            Id = id;
-            Name = name;
-            borrowed_books = new int[5] ;
-            count = 0;
-        }
-        
-        public override string ToString()
-        {
-            return $"ID : {Id} Name is : {Name} borrowed_books : {count}";
-        }
-    }
+     public class Member 
+     {
+         public int ID;
+         public string Name;
+         public int[] BorrowedBooks;
+         public int BorrowCount ;
+         public Member(int id , string name)
+         {
+             ID= id ;
+            Name = name ;
+            BorrowedBooks = new int[10];
+            borrowedCount = 0;
+         }
+          public override string ToString()
+         {
+              return $"ID: {ID}, Name: {Name}, Borrowed Books: {BorrowedCount}";
+              
+         }
+         public void MeBorrowBook(int id)
+         {
+             
+             if (BorrowedCount<BorrowedBooks.Length)
+             {
+                 BorrowedBooks[borrowedCount]=ID ;
+                 borrowedCount++;
+             }
+         }
+          public void RetReturnBook(int BookId) 
+          {
+              int index = -1;
+              for(int i= 0; i < BorrowedCount; i++)
+              {
+                  if (BorrowedBooks[i] ==BookId)
+                  {
+                      index = i ;
+                      break;
+                  }
+  
+              }
+              if(index!=-1)
+              {
+                  for(int i =(int i = index; i < BorrowedCount - 1; i++)
+                  {
+                       BorrowedBooks[i] = BorrowedBooks[i+1] ;
+                  }
+                  BorrowedCount--;    
+              }
+              
+          }
+         
+         
+     }
+   
 }
