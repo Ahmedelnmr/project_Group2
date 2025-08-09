4ba852f2a51ce2c13077cf5aac38ee9fbcde4a5a
bassemaboulgheit <basem.abulgheit@gmail.com>
2025-08-09 19:37:42 +0300

    Update Library.cs

--- project_Group2/Library.cs
+++ project_Group2/Library.cs
@@ -13,5 +13,19 @@
             books_count = 0;
             members_count = 0;
         }
+        // Add Member
+        public void AddMember(Member member)
+        {
+            if (members_count >= members.Length)
+            {
+                Console.WriteLine("your library is full of members");
+            }
+            else
+            {
+                members[members_count] = member;
+                Console.WriteLine("member added successfully");
+                members_count++;
+            }
+        }
     }
 }
