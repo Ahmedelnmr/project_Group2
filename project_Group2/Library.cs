bfc5e0142267ca9a615e41ce5cbbe93f53d7e57d
Ahmed Elnmer <153692962+Ahmedelnmr@users.noreply.github.com>
2025-08-09 23:45:41 +0300

    books_methods

--- project_Group2/Library.cs
+++ project_Group2/Library.cs
@@ -13,7 +13,75 @@
             books_count = 0;
             members_count = 0;
         }
-        // Add Member
+//-------------AddBook------------------//
+         public void AddBook(Book book)
+             {
+                  
+                 if (books_count >= books.Length)
+                 {
+                     Console.WriteLine("your library is full of books");
+                 }
+               
+                 else
+                 {
+                     if (books_count != 0)
+                     {
+                         for (int i = 0; i < books_count; i++)
+                         {
+                             if (books[i].Equals(book))
+                             {
+                                 Console.WriteLine("the id of book should be unique");
+                                 return;
+                             }
+                         }
+                     }
+                     books[books_count] = book;
+                     Console.WriteLine("Book added sucessfully");
+                     books_count++;
+            
+                 }
+             }
+//-------------RemoveBook------------------//
+             public void RemoveBook(int id)
+             {
+                 int index = FindBooks(id);
+                 if (index == -1)
+                 {
+                     Console.WriteLine("the book is not found");
+            
+                 }
+                 else
+                 {
+                     if (!books[index].IsAvailable)
+                     {
+                         Console.WriteLine("The book is borrowed ");
+                         return;
+            
+                     }
+                     books[index] = books[books_count-1];
+                     books[books_count-1] = null;
+                     books_count--;
+                     Console.WriteLine("Book deleted sucessfully");
+            
+                 }
+            
+             }
+//-----------------FindBooks---------------------//
+         public int FindBooks(int id)
+         {
+             for (int i = 0; i < books_count; i++)
+             {
+                 if (books[i].Id ==id)
+                 {
+                     return i;
+                 }
+        
+             }
+             return -1;
+        
+        
+         }
+                // Add Member
         public void AddMember(Member member)
         {
             if (members_count >= members.Length)
